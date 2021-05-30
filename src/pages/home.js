78df1b622ec941472c40e9233805c49aa5dc6bf4
Victor A. Banuelos <vbanuelos@caa.com>
2021-05-29 17:37:29 -0700

    Add images

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,7 @@
 import React from "react";
-import { Grid, Header, Image } from "semantic-ui-react";
+import { Grid, Header } from "semantic-ui-react";
 import Navbar from "../components/Navbar";
 import ContentSegment from "../components/ContentSegment";
-import heroImg from "/src/images/banner-commercial.jpg";
-import adrianImg from "/src/images/adrian2.jpg";
-
-const heroDivStyle = {
-  backgroundImage: `url(${heroImg})`,
-  backgroundSize: "cover",
-  height: "20em"
-};
 
 const hours = [
   { date: "Monday:",     hours: "9:00am - 7:00pm" },
@@ -18,14 +10,14 @@ const hours = [
   { date: "Thursday:",   hours: "9:00am - 7:00pm" },
   { date: "Friday:",     hours: "9:00am - 7:00pm" },
   { date: "Saturday:",   hours: "10:00am - 3:00pm" },
-  { date: "Sunday:",     hours: "10:00am - 3:00pm" },
+  { date: "Friday:",     hours: "10:00am - 3:00pm" },
 ];
 
 const dataToGrid = (leftData, rightData) => {
   return (
-    <Grid.Row key={leftData} style={{padding: ".5em 0"}}>
-      <Grid.Column width="8" textAlign='center'><strong>{leftData}</strong></Grid.Column>
-      <Grid.Column width="8" textAlign='center'>{rightData}</Grid.Column>
+    <Grid.Row style={{padding: ".5em 0"}}>
+      <Grid.Column width="8" textAlign='right'><strong>{leftData}</strong></Grid.Column>
+      <Grid.Column width="8" textAlign='left'>{rightData}</Grid.Column>
     </Grid.Row>
   );
 };
@@ -47,16 +39,7 @@ const homepage = () => {
         <Header size="large" textAlign="center" style={{ marginTop: 0 }}>Open today until {openTill()}</Header>
       </ContentSegment>
       <ContentSegment id="image" showDivider={true}>
-        <div style={heroDivStyle}>
-          <Image
-            alt="Adrian"
-            src={adrianImg}
-            size='medium'
-            circular
-            style={{ width: "250px", float: "right", position: "relative", top: "1.25em", right: "1.25em" }}
-            verticalAlign="middle"
-          />
-        </div>
+        add image
       </ContentSegment>
       <ContentSegment id="reviews" showDivider={true}>
         add yelp reviews
@@ -66,12 +49,12 @@ const homepage = () => {
       </ContentSegment>
       <ContentSegment id="contactInfo">
         <Header size="large" textAlign="center" style={{ marginBottom: "2em" }}>Business Info</Header>
-        <Grid stackable>
+        <Grid>
           <Grid.Row>
             <Grid.Column width={8}>
               <Grid>
                 <Grid.Row>
-                  <Grid.Column width="16" textAlign="center"><h3>Contact Info:</h3></Grid.Column>
+                  <Grid.Column width="16" textAlign="center" style={{paddingLeft: "2em"}}><h3>Contact Info:</h3></Grid.Column>
                 </Grid.Row>
                 {dataToGrid("Cell Phone", "(323) 765 - 4321")}
                 {dataToGrid("Email", "someEmail@gmail.com")}
@@ -80,7 +63,7 @@ const homepage = () => {
             <Grid.Column width={8}>
               <Grid>
                 <Grid.Row >
-                  <Grid.Column width="16" textAlign="center"><h3>Business Hours:</h3></Grid.Column>
+                  <Grid.Column width="16" textAlign="center" style={{paddingLeft: "3em"}}><h3>Business Hours:</h3></Grid.Column>
                 </Grid.Row>
                 { hours.map(h => dataToGrid(h.date, h.hours)) }
               </Grid>
